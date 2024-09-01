9252ea78da01475852d7d0e31852f266b5d59eff
Ivanilso da Silva Conceicão Filho <ivanilsodasilva300@gmail.com>
2024-09-01 13:06:43 -0300

    FEAT: Melhoramento da saida

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
             {1, 0, 0, 1},
             {0, 1, 1, 0}
         };
-        
+
         double[][] saidas = {
         //	{Gripe, Dengue}
             {1, 0},
@@ -20,11 +20,33 @@ public class Main {
         MLP mlp = new MLP(4, 2, 2, 0.5);
         mlp.treinar(entradas, saidas, 10000);
 
-        // Testar a rede com uma nova entrada
-        double[] entradaTeste = {1, 0, 1, 0}; // Deve resultar em gripe
-        double[] resultado = mlp.prever(entradaTeste);
+        // Testar a rede com novas entradas
+        double[] entradaTeste1 = {1, 0, 1, 0};
+        double[] resultado1 = mlp.prever(entradaTeste1);
+        System.out.println();
+        System.out.println("Entrada: {1, 0, 1, 0}");
+        System.out.println("Gripe: " + resultado1[0]);
+        System.out.println("Dengue: " + resultado1[1]);
 
-        System.out.println("Gripe: " + resultado[0]);
-        System.out.println("Dengue: " + resultado[1]);
+        double[] entradaTeste2 = {0, 1, 0, 1};
+        double[] resultado2 = mlp.prever(entradaTeste2);
+        System.out.println();
+        System.out.println("Entrada: {0, 1, 0, 1}");
+        System.out.println("Gripe: " + resultado2[0]);
+        System.out.println("Dengue: " + resultado2[1]);
+
+        double[] entradaTeste3 = {1, 0, 0, 1};
+        double[] resultado3 = mlp.prever(entradaTeste3);
+        System.out.println();
+        System.out.println("Entrada: {1, 0, 0, 1}");
+        System.out.println("Gripe: " + resultado3[0]);
+        System.out.println("Dengue: " + resultado3[1]);
+
+        double[] entradaTeste4 = {0, 1, 1, 0};
+        double[] resultado4 = mlp.prever(entradaTeste4);
+        System.out.println();
+        System.out.println("Entrada: {0, 1, 1, 0}");
+        System.out.println("Gripe: " + resultado4[0]);
+        System.out.println("Dengue: " + resultado4[1]);
     }
 }
